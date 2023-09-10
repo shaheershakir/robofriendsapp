0ef356218886d3020dafda3007e20ece0fc2b9f8
Shaheer Shakir <shaheer@pop-os.localdomain>
2023-09-10 13:39:38 +0530

    add overFlow scroll and optimized the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardList from "./CardList"; // import the Card component
 import SearchField from "./SearchBox"; // import the SearchField component
-import { robots } from '../robots'
+import Scroll from "./Scroll"; // import the Scroll component
+import "../App.css";
 
 function App () {
-    const [robot, setRobot] = useState(robots)
+    const [robot, setRobot] = useState([])
     const [searchField, setsearchField] = useState('')
     
     const filteredRobots = robot.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
-    
+
     function onSearchChange(event) {
       setsearchField(event.target.value)
     }
 
+    useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {setRobot(users)})
+    })
 
-  return (
-    <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchField searchChange={onSearchChange} />
-        <CardList robots={filteredRobots} />
-    </div>
-  );
+     return !robot.length ? (
+      <h1>Loading</h1>
+      ) : (
+          <div className="tc">
+              <h1>RoboFriends</h1>
+              <SearchField searchChange={onSearchChange} />
+              <Scroll>
+                <CardList robots={filteredRobots} />
+              </Scroll>
+          </div>
+       );   
 }
 
 export default App;
