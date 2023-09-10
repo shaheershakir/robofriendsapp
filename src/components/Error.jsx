b5a9fd6e259b0aa36fd873e7a413a2203b62a653
Shaheer Shakir <shaheer@pop-os.localdomain>
2023-09-10 15:35:02 +0530

    handleError spell correct

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,11 +4,11 @@ function Error(props) {
     const [hasError, setErrors] = useState(false);
 
     useEffect(() => {
-        const handdleError = () => {
+        const handleError = () => {
             setErrors(true);
         };
-        window.addEventListener("error", handdleError);
-        return () => window.removeEventListener("error", handdleError);
+        window.addEventListener("error", handleError);
+        return () => window.removeEventListener("error", handleError);
     }, []);
 
   return hasError ? ( <h1>Ooops Something went wrong, That's not good</h1> ) : ( <div>{props.children}</div> );
